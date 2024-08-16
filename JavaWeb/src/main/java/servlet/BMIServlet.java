c1ee6d9df921fccb0576f9f03054ec0613fc69b2
teacher <teacher@BE-403->
2024-08-16 21:27:38 +0800

    java

--- JavaWeb/src/main/java/servlet/BMIServlet.java
+++ JavaWeb/src/main/java/servlet/BMIServlet.java
@@ -3,10 +3,12 @@ package servlet;
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@WebServlet("/servlet/bmi")
 public class BMIServlet extends HttpServlet {
 
 	@Override
