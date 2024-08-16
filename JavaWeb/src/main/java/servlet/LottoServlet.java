4fcf9e90679191e7b9e807cf5fa83f81f4c312f9
teacher <teacher@BE-403->
2024-08-16 20:47:09 +0800

    web

--- JavaWeb/src/main/java/servlet/LottoServlet.java
+++ JavaWeb/src/main/java/servlet/LottoServlet.java
@@ -19,19 +19,25 @@ public class LottoServlet extends HttpServlet {
 		
 		String countStr = req.getParameter("count"); // 取得 count 參數內容
 		
+		int count = 1; // 欲取得電腦選號的次數
+		if(countStr != null && countStr.trim().length() > 0) { // countStr 不是 null , countStr 有內容 
+			count = Integer.parseInt(countStr); // 字串轉數字
+		}
 		
 		Random random = new Random(); // 隨機數物件
-		// 1~39 取 n 個不重複的號碼
-		int n = 5;
-		Set<Integer> lotto = new LinkedHashSet(); // 存放不重複的樂透號碼集合
-		while (lotto.size() < n) {
-			int number = random.nextInt(39) + 1;
-			lotto.add(number);
-		}
-		// Response 回應
-		resp.getWriter().print("countStr: " + countStr);
-		//resp.getWriter().print("Hello Lotto: " + lotto);
 		
+		for(int i=0;i<count;i++) {
+			// 1~39 取 n 個不重複的號碼
+			int n = 5;
+			Set<Integer> lotto = new LinkedHashSet(); // 存放不重複的樂透號碼集合
+			while (lotto.size() < n) {
+				int number = random.nextInt(39) + 1;
+				lotto.add(number);
+			}
+			// Response 回應
+			//resp.getWriter().print("countStr: " + countStr);
+			resp.getWriter().print("Lotto: " + lotto);
+		}
 	}
 	
 }
