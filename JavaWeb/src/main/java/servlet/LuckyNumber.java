6aee4dbcc411597347d8df71871f61d013e49ddf
teacher <teacher@BE-403->
2024-08-19 19:08:03 +0800

    web

---
+++ JavaWeb/src/main/java/servlet/LuckyNumber.java
@@ -0,0 +1,11 @@
+package servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = {"/servlet/lucky", "/my/number"})
+public class LuckyNumber extends HttpServlet {
+	
+	
+	
+}
