691d946bb569888be0ba235740d4cd244b288cec
teacher <teacher@BE-403->
2024-08-19 19:11:57 +0800

    web

--- JavaWeb/src/main/java/servlet/LuckyNumber.java
+++ JavaWeb/src/main/java/servlet/LuckyNumber.java
@@ -1,11 +1,21 @@
 package servlet;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = {"/servlet/lucky", "/my/number"})
 public class LuckyNumber extends HttpServlet {
-	
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
 	
 	
 }
