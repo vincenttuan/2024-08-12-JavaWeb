65373bd5f20b3d9020863a3527b1b3ab827c19eb
teacher <teacher@BE-403->
2024-08-23 19:17:03 +0800

    java

--- JavaWeb/src/main/java/guestbook/controller/AddGuestbookServlet.java
+++ JavaWeb/src/main/java/guestbook/controller/AddGuestbookServlet.java
@@ -1,5 +1,19 @@
 package guestbook.controller;
 
-public class AddGuestbookServlet {
+import java.io.IOException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/guestbook/add")
+public class AddGuestbookServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().print("Guestbook Add");
+	}
+	
 }
