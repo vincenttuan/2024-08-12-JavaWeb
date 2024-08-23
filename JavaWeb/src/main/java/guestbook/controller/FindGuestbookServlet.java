6d736a9375e827a9c207a60a7cf1ab4ec6388507
teacher <teacher@BE-403->
2024-08-23 20:26:20 +0800

    java

---
+++ JavaWeb/src/main/java/guestbook/controller/FindGuestbookServlet.java
@@ -0,0 +1,29 @@
+package guestbook.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import guestbook.dao.GuestbookDao;
+import guestbook.entity.Guestbook;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/guestbook/find")
+public class FindGuestbookServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 查詢所有留言紀錄
+		GuestbookDao dao = new GuestbookDao();
+		List<Guestbook> guestbooks = dao.findAllGuestbooks();
+		
+		// 將所有留言轉成 json 格式後印出
+		
+		
+		
+	}
+	
+}
