143bf99e9c23073cda20aa6d0f10d9635a61d833
teacher <teacher@BE-403->
2024-08-26 19:37:56 +0800

    web

--- JavaWeb/src/main/java/employee/controller/EmployeeServlet.java
+++ JavaWeb/src/main/java/employee/controller/EmployeeServlet.java
@@ -1,5 +1,23 @@
 package employee.controller;
 
-public class EmployeeServlet {
+import java.io.IOException;
+import java.util.List;
 
+import employee.dao.EmployeeDao;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class EmployeeServlet extends HttpServlet {
+	
+	private EmployeeDao dao = new EmployeeDao();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		List<String> employees 
+		
+	}
+	
 }
--- JavaWeb/src/main/java/employee/dao/EmployeeDao.java
+++ JavaWeb/src/main/java/employee/dao/EmployeeDao.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 public class EmployeeDao {
 	// 員工資料
-	private static List<String> employees = List.of("John", "Mary", "Jack", "Rose", "Helen");
+	private static List<String> employeeNames = List.of("John", "Mary", "Jack", "Rose", "Helen");
 	
 	// 查詢全部
-	public List<String> findAll() {
-		return employees;
+	public List<String> findAllNames() {
+		return employeeNames;
 	}
 	
 }
