264960d86149f2c36bf91a7f5e304e94397e7219
teacher <teacher@BE-403->
2024-08-26 21:39:23 +0800

    web

---
+++ JavaWeb/src/main/java/employee/entity/EmployeeName.java
@@ -0,0 +1,21 @@
+package employee.entity;
+
+// 對應 employee_name 資料表欄位
+public class EmployeeName {
+	private Integer id;
+	private String name;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
