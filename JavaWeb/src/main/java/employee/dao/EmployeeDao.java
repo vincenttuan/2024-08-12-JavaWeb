2bae3ad5c2073d4207a7a436d65a308a649cf3d2
teacher <teacher@BE-403->
2024-08-26 19:38:20 +0800

    web

--- JavaWeb/src/main/java/employee/dao/EmployeeDao.java
+++ JavaWeb/src/main/java/employee/dao/EmployeeDao.java
@@ -3,10 +3,10 @@ package employee.dao;
 import java.util.List;
 
 public class EmployeeDao {
-	// 員工資料
+	// 員工姓名資料
 	private static List<String> employeeNames = List.of("John", "Mary", "Jack", "Rose", "Helen");
 	
-	// 查詢全部
+	// 查詢全部員工姓名
 	public List<String> findAllNames() {
 		return employeeNames;
 	}
