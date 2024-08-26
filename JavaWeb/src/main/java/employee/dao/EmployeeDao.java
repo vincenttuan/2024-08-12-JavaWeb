6a7ef26bd65b8dc9395588e1aff5cf8db56ee114
teacher <teacher@BE-403->
2024-08-26 21:10:16 +0800

    web

--- JavaWeb/src/main/java/employee/dao/EmployeeDao.java
+++ JavaWeb/src/main/java/employee/dao/EmployeeDao.java
@@ -2,6 +2,9 @@ package employee.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeDao {
@@ -19,9 +22,24 @@ public class EmployeeDao {
 		}
 	}
 	
-	
 	// 查詢全部員工姓名
 	public List<String> findAllNames() {
+		String sql = "select name from employee_name";
+		try(Statement stmt = conn.createStatement();  // 建立 sql 敘述物件
+			ResultSet rs = stmt.executeQuery(sql);) {
+			// 準備一個 List<String> 用來存放 name
+			List<String> employeeNames = new ArrayList<>();
+			// 輪詢資料表中每一筆紀錄, 並加入到集合中
+			while (rs.next()) {
+				String name = rs.getString("name");
+				employeeNames.add(name);
+			}
+			return employeeNames;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
 		return null;
 	}
 	
