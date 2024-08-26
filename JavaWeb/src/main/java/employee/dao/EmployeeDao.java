08d8075297b358d94aeb915651b7a3a826b099ce
teacher <teacher@BE-403->
2024-08-26 21:01:23 +0800

    web

--- JavaWeb/src/main/java/employee/dao/EmployeeDao.java
+++ JavaWeb/src/main/java/employee/dao/EmployeeDao.java
@@ -1,14 +1,21 @@
 package employee.dao;
 
+import java.sql.Connection;
 import java.util.List;
 
 public class EmployeeDao {
-	// 員工姓名資料
-	private static List<String> employeeNames = List.of("John", "Mary", "Jack", "Rose", "Helen");
+	private static Connection conn;
+	
+	static {
+		String username = "root";
+		String password = "12345678";
+		String dbUrl = "jdbc:mysql://localhost:3306/web?serverTimezone=Asia/Taipei&characterEncoding=utf-8&useUnicode=true";
+	}
+	
 	
 	// 查詢全部員工姓名
 	public List<String> findAllNames() {
-		return employeeNames;
+		return null;
 	}
 	
 }
