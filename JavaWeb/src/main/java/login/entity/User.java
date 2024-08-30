3871ab4a7330964bb94d026e4c72f41992f08ec5
teacher <teacher@BE-403->
2024-08-30 20:01:16 +0800

    web

--- JavaWeb/src/main/java/login/entity/User.java
+++ JavaWeb/src/main/java/login/entity/User.java
@@ -2,5 +2,54 @@ package login.entity;
 
 // user 資料表紀錄
 public class User {
+	private Integer userId;
+	private String userName;
+	private String passwordHash;
+	private String salt;
+	private String email;
+	private Boolean active;
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+	public String getSalt() {
+		return salt;
+	}
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Boolean getActive() {
+		return active;
+	}
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userName=" + userName + ", passwordHash=" + passwordHash + ", salt=" + salt
+				+ ", email=" + email + ", active=" + active + "]";
+	}
+	
 	
 }
