17a62c3e2e86f88307f9df4c56b0d0c1ae31214e
teacher <teacher@BE-403->
2024-09-02 20:44:01 +0800

    web

--- JavaWeb/src/main/test/test/InsertUser.java
+++ JavaWeb/src/main/test/test/InsertUser.java
@@ -2,10 +2,19 @@ package test;
 
 import org.junit.jupiter.api.Test;
 
+import login.service.UserService;
+
 public class InsertUser {
 	
 	@Test
 	public void add() {
+		String userName = "admin";
+		String password = "1234";
+		String email = "admin@gmail.com";
+		
+		UserService userService = UserService.getInstance();
+		boolean addResult = userService.insertUser(userName, password, email);
+		System.out.printf("測試結果 addResult: %s%n", addResult);
 		
 	}
 	
