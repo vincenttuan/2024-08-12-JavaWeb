c8c3523fe6b9dc9f394c005929bc4942b580d28f
teacher <teacher@BE-403->
2024-09-06 19:17:26 +0800

    web

--- JavaWeb/src/main/java/login/controller/RegisterServlet.java
+++ JavaWeb/src/main/java/login/controller/RegisterServlet.java
@@ -2,6 +2,9 @@ package login.controller;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,7 +14,9 @@ import jakarta.servlet.http.HttpServletResponse;
 // 使用者註冊
 @WebServlet("/user/register")
 public class RegisterServlet extends HttpServlet {
-
+	
+	private static final Logger logger = LoggerFactory.getLogger(RegisterServlet.class);
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 接收使用者所傳的資料
