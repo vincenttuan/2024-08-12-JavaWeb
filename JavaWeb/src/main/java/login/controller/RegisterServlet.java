2a111975d49cce0020e2a0a185bc07bf66acfeb4
teacher <teacher@BE-403->
2024-09-06 19:04:07 +0800

    web

--- JavaWeb/src/main/java/login/controller/RegisterServlet.java
+++ JavaWeb/src/main/java/login/controller/RegisterServlet.java
@@ -1,6 +1,27 @@
 package login.controller;
 
-// 使用者註冊
-public class RegisterServlet {
+import java.io.IOException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+// 使用者註冊
+@WebServlet("/user/register")
+public class RegisterServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 接收使用者所傳的資料
+		String userName = req.getParameter("userName");
+		String password = req.getParameter("password");
+		String email = req.getParameter("email");
+		
+		// 將所收到的資料寫入 Log 以便日後 debug 或追蹤紀錄使用
+		
+		
+	}
+	
 }
