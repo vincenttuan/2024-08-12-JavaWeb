3516121404850bcf1e2ca96204cd9ca0057c0801
teacher <teacher@BE-403->
2024-09-06 19:43:21 +0800

    java

--- JavaWeb/src/main/java/login/controller/RegisterServlet.java
+++ JavaWeb/src/main/java/login/controller/RegisterServlet.java
@@ -10,6 +10,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import login.service.UserService;
 
 // 使用者註冊
 @WebServlet("/user/register")
@@ -17,15 +18,22 @@ public class RegisterServlet extends HttpServlet {
 	
 	private static final Logger logger = LoggerFactory.getLogger(RegisterServlet.class);
 	
+	private UserService userService = UserService.getInstance();
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// 接收使用者所傳的資料
+		// 1.接收使用者所傳的資料
 		String userName = req.getParameter("userName");
 		String password = req.getParameter("password");
 		String email = req.getParameter("email");
 		
 		// 將所收到的資料寫入 Log 以便日後 debug 或追蹤紀錄使用
 		logger.info("userName: " + userName + ", password: " + password + ", email: " + email);
+		
+		// 透過 userService 將使用者資料加密並存入到資料表
+		boolean checkResult = userService.insertUser(userName, password, email);
+		logger.info("checkResult: " + checkResult);
+		
 		
 	}
 	
