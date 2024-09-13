0b6554dae9471f344b83849c32215bf9e25ac904
teacher <teacher@BE-403->
2024-09-13 19:28:16 +0800

    java

--- JavaWeb/src/main/java/login/controller/UserServlet.java
+++ JavaWeb/src/main/java/login/controller/UserServlet.java
@@ -1,5 +1,29 @@
 package login.controller;
 
-public class UserServlet {
+import java.io.IOException;
+import java.util.List;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import login.entity.User;
+import login.service.UserService;
+
+@WebServlet("/user/list")
+public class UserServlet extends HttpServlet {
+	// 取得 UserService 的實體物件
+	private UserService userService = UserService.getInstance();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 得到所有使用者資料
+		List<User> users = userService.findAllUsers();
+		// 將 users 放到 request 物件屬性中, 以便傳給 user.jsp 使用
+		req.setAttribute("users", users);
+		// 傳送到 user.jsp
+		req.getRequestDispatcher("/WEB-INF/jsp/login/user.jsp").forward(req, resp);
+	}
+	
 }
