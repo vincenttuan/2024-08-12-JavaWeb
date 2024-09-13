3b9f5bbfb054a55905e9c935e5e7990376d8efa8
teacher <teacher@BE-403->
2024-09-13 20:52:26 +0800

    web

--- JavaWeb/src/main/java/login/controller/LoginServlet.java
+++ JavaWeb/src/main/java/login/controller/LoginServlet.java
@@ -1,6 +1,21 @@
 package login.controller;
 
-// 使用者登入
-public class LoginServlet {
+import java.io.IOException;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+// 使用者登入
+@WebServlet("/user/login")
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 重導到指定 login.jsp 表單畫面
+		req.getRequestDispatcher("/WEB-INF/jsp/login/login.jsp").forward(req, resp);
+	}
+	
 }
