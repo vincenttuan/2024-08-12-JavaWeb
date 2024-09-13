7c8a0b4c0a6eed4ad7a76d695edbc944d708c4af
teacher <teacher@BE-403->
2024-09-13 21:28:40 +0800

    java

--- JavaWeb/src/main/java/login/controller/LoginServlet.java
+++ JavaWeb/src/main/java/login/controller/LoginServlet.java
@@ -7,11 +7,16 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import login.dto.UserDto;
+import login.exception.LoginException;
+import login.service.UserService;
 
 // 使用者登入
 @WebServlet("/user/login")
 public class LoginServlet extends HttpServlet {
-
+	
+	private UserService userService = UserService.getInstance();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 重導到指定 login.jsp 表單畫面
@@ -24,7 +29,15 @@ public class LoginServlet extends HttpServlet {
 		String password = req.getParameter("password");
 		
 		// 比對登入資訊
-		
+		try {
+			UserDto userDto = userService.verifyUser(userName, password);
+			req.setAttribute("userDto", userDto);
+			req.setAttribute("loginSuccess", true);
+			req.setAttribute("loginMessage", "登入成功");
+		} catch (LoginException e) {
+			req.setAttribute("loginSuccess", false);
+			req.setAttribute("loginMessage", "登入失敗");
+		}
 		
 	}
 	
