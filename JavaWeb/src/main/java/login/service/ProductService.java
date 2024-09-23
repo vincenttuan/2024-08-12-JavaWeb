9bfc6aa25fa6dd7a33e07d6303d3727e0928c2f2
teacher <teacher@BE-403->
2024-09-23 20:02:26 +0800

    web

--- JavaWeb/src/main/java/login/service/ProductService.java
+++ JavaWeb/src/main/java/login/service/ProductService.java
@@ -2,14 +2,27 @@ package login.service;
 
 import login.dao.ProductDao;
 import login.dao.ProductDaoImpl;
+import login.dto.ProductDto;
 import login.entity.Product;
 
 public class ProductService {
 	
 	private ProductDao productDao = new ProductDaoImpl();
 	
+	// 取得 ProductDto 物件資料
 	public ProductDto getById(int productId) {
-		return productDao.getById(productId);
+		// 自 productDao 得到 Product 物件
+		Product product = productDao.getById(productId);
+		if(product == null) {
+			return null;
+		}
+		// 將 Product 物件轉 ProductDto 物件
+		ProductDto productDto = new ProductDto();
+		productDto.setId(product.getProductId());
+		productDto.setName(product.getProductName());
+		productDto.setPrice(product.getPrice());
+		productDto.setQty(product.getStockQuantity());
+		return productDto;
 	}
 	
 }
