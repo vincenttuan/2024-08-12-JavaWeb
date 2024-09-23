3d65d7510c37153b98d1d370f942f3e0fff41f81
teacher <teacher@BE-403->
2024-09-23 19:10:34 +0800

    web

---
+++ JavaWeb/src/main/java/login/dao/BaseDao.java
@@ -0,0 +1,21 @@
+package login.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class BaseDao {
+	protected static Connection conn;
+	
+	static {
+		String username = "root";
+		String password = "12345678";
+		String dbUrl = "jdbc:mysql://localhost:3306/web?serverTimezone=Asia/Taipei&characterEncoding=utf-8&useUnicode=true";
+		// 建立連線
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(dbUrl, username, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ JavaWeb/src/main/java/login/dao/ProductDaoImpl.java
@@ -0,0 +1,22 @@
+package login.dao;
+
+import java.util.List;
+
+import login.entity.Product;
+
+public class ProductDaoImpl implements ProductDao {
+	
+	
+	@Override
+	public Product getById(int productId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> findAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
