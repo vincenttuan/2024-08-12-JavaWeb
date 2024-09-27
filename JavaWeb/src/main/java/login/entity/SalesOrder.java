261c32c62d03fbf577915f759bb065f9b69ee6b8
teacher <teacher@BE-403->
2024-09-27 19:07:52 +0800

    web

---
+++ JavaWeb/src/main/java/login/entity/SalesOrder.java
@@ -0,0 +1,45 @@
+package login.entity;
+
+import java.util.Date;
+
+public class SalesOrder {
+	private Integer orderId;
+	private Integer customerId;
+	private Date orderDate;
+	private Double totalAmount;
+	private String orderStatus;
+	
+	public Integer getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+	public Integer getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+	public Date getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+	public Double getTotalAmount() {
+		return totalAmount;
+	}
+	public void setTotalAmount(Double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	
+	
+	
+}
