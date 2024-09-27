adc85189d4ef4383168a66d1c310b352ff193def
teacher <teacher@BE-403->
2024-09-27 21:16:36 +0800

    web

--- JavaWeb/src/main/java/login/service/OrderItemService.java
+++ JavaWeb/src/main/java/login/service/OrderItemService.java
@@ -6,12 +6,25 @@ import java.util.List;
 import login.dao.OrderItemDao;
 import login.dao.OrderItemDaoImpl;
 import login.dto.OrderItemDto;
+import login.entity.OrderItem;
 
 public class OrderItemService {
 	private OrderItemDao orderItemDao = new OrderItemDaoImpl();
 	
 	public List<OrderItemDto> findAllOrderItemsByOrderId(Integer orderId) {
 		List<OrderItemDto> orderItemDtos = new ArrayList<>();
+		List<OrderItem> orderItems = orderItemDao.findAllOrderItemsByOrderId(orderId);
+		for(OrderItem orderItem : orderItems) {
+			OrderItemDto orderItemDto = new OrderItemDto();
+			orderItemDto.setItemId(orderItem.getItemId());
+			orderItemDto.setOrderId(orderItem.getOrderId());
+			orderItemDto.setProductId(orderItem.getProductId());
+			orderItemDto.setProductName(null);
+			orderItemDto.setQuantity(orderItem.getQuantity());
+			orderItemDto.setUnitPrice(orderItem.getUnitPrice());
+			// 注入到 orderItemDtos 集合
+			orderItemDtos.add(orderItemDto);
+		}
 		
 		return orderItemDtos;
 	}
