fd02281251cb9313f1f68e88178821a479e454b1
teacher <teacher@BE-403->
2024-09-27 19:47:38 +0800

    web

---
+++ JavaWeb/src/main/java/login/service/SalesOrderService.java
@@ -0,0 +1,34 @@
+package login.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import login.dao.SalesOrderDao;
+import login.dao.SalesOrderDaoImpl;
+import login.dao.UserDao;
+import login.dto.SalesOrderDto;
+import login.entity.SalesOrder;
+
+public class SalesOrderService {
+	private SalesOrderDao salesOrderDao = new SalesOrderDaoImpl();
+	private UserDao userDao = new UserDao();
+	
+	public List<SalesOrderDto> findAllSalesOrderDtos() {
+		List<SalesOrderDto> salesOrderDtos = new ArrayList<>();
+		// 取得所有 salesOrders
+		List<SalesOrder> salesOrders = salesOrderDao.findAllSalesOrders();
+		// 將每一筆 SalesOrder 物件屬性資料對應到 SalesOrderDto 物件屬性中
+		for(SalesOrder salesOrder : salesOrders) {
+			SalesOrderDto salesOrderDto = new SalesOrderDto();
+			salesOrderDto.setOrderId(salesOrder.getOrderId());
+			salesOrderDto.setUserId(salesOrder.getCustomerId());
+			salesOrderDto.setUserName(null);
+			salesOrderDto.setOrderDate(salesOrder.getOrderDate());
+			salesOrderDto.setTotalAmount(salesOrder.getTotalAmount());
+			salesOrderDto.setOrderStatus(salesOrder.getOrderStatus());
+			// 注入到 salesOrderDtos 集合中
+			salesOrderDtos.add(salesOrderDto);
+		}
+		return salesOrderDtos;
+	}
+}
