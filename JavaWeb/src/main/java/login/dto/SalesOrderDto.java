3d4e68ae03c8af90b7c3944d0f801d98f72ea2c0
teacher <teacher@BE-403->
2024-09-27 19:17:06 +0800

    web

--- JavaWeb/src/main/java/login/dto/SalesOrderDto.java
+++ JavaWeb/src/main/java/login/dto/SalesOrderDto.java
@@ -10,6 +10,43 @@ public class SalesOrderDto {
 	private Date    orderDate;   // 對應 SalesOrder.orderDate
 	private Double  totalAmount; // 對應 SalesOrder.totalAmount
 	private String  orderStatus; // 對應 SalesOrder.orderStatus
+	public Integer getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public Date getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+	public Double getTotalAmount() {
+		return totalAmount;
+	}
+	public void setTotalAmount(Double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	
 	
 	
 }
