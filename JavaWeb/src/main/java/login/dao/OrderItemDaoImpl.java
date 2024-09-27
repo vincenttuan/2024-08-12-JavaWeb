61c0da7bdb8e48b3c15601957dc681968ade4f77
teacher <teacher@BE-403->
2024-09-27 21:00:05 +0800

    web

---
+++ JavaWeb/src/main/java/login/dao/OrderItemDaoImpl.java
@@ -0,0 +1,15 @@
+package login.dao;
+
+import java.util.List;
+
+import login.entity.OrderItem;
+
+public class OrderItemDaoImpl extends BaseDao implements OrderItemDao {
+
+	@Override
+	public List<OrderItem> findAllOrderItemsByOrderId(Integer orderId) {
+		
+		return null;
+	}
+	
+}
