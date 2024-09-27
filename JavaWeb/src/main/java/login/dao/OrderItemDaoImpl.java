628fb7d7563b5c0ae4fe49b751631c0154eff368
teacher <teacher@BE-403->
2024-09-27 21:02:52 +0800

    web

--- JavaWeb/src/main/java/login/dao/OrderItemDaoImpl.java
+++ JavaWeb/src/main/java/login/dao/OrderItemDaoImpl.java
@@ -1,5 +1,7 @@
 package login.dao;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import login.entity.OrderItem;
@@ -8,8 +10,14 @@ public class OrderItemDaoImpl extends BaseDao implements OrderItemDao {
 
 	@Override
 	public List<OrderItem> findAllOrderItemsByOrderId(Integer orderId) {
+		List<OrderItem> orderItems = new ArrayList<>();
 		String sql = "select item_id, order_id, product_id, quantity, unit_price from order_item where order_id = ?";
-		return null;
+		try() {
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return orderItems;
 	}
 	
 }
