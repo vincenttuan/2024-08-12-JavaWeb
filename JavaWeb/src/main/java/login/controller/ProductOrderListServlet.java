e8aaf33938eb2e7956a8f0121283c596f7d23660
teacher <teacher@BE-403->
2024-09-27 19:53:12 +0800

    web

--- JavaWeb/src/main/java/login/controller/ProductOrderListServlet.java
+++ JavaWeb/src/main/java/login/controller/ProductOrderListServlet.java
@@ -1,19 +1,26 @@
 package login.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import login.dto.SalesOrderDto;
+import login.service.SalesOrderService;
 
 @WebServlet("/product/order/list")
 public class ProductOrderListServlet extends HttpServlet {
-
+	
+	private SalesOrderService salesOrderService = new SalesOrderService();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().print("ProductOrderListServlet not ready !");
+		List<SalesOrderDto> salesOrderDtos = salesOrderService.findAllSalesOrderDtos();
+		req.setAttribute("salesOrderDtos", salesOrderDtos);
+		req.getRequestDispatcher("/WEB-INF/jsp/login/order_list.jsp");
 	}
 	
 }
