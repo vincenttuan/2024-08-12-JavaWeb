786f140ab2c9d45f43ea857b3e856553081e69a0
teacher <teacher@BE-403->
2024-09-27 21:10:26 +0800

    web

--- JavaWeb/src/main/java/login/dao/OrderItemDaoImpl.java
+++ JavaWeb/src/main/java/login/dao/OrderItemDaoImpl.java
@@ -1,5 +1,7 @@
 package login.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +14,23 @@ public class OrderItemDaoImpl extends BaseDao implements OrderItemDao {
 	public List<OrderItem> findAllOrderItemsByOrderId(Integer orderId) {
 		List<OrderItem> orderItems = new ArrayList<>();
 		String sql = "select item_id, order_id, product_id, quantity, unit_price from order_item where order_id = ?";
-		try() {
+		try(PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			
+			pstmt.setInt(1, orderId);
+			try(ResultSet rs = pstmt.executeQuery()) {
+				// 資料逐筆取出
+				while (rs.next()) {
+					// 建立 OrderItem 物件並放入相關資料
+					OrderItem orderItem = new OrderItem();
+					orderItem.setItemId(rs.getInt("item_id"));
+					orderItem.setOrderId(rs.getInt("order_id"));
+					orderItem.setProductId(rs.getInt("product_id"));
+					orderItem.setQuantity(rs.getInt("quantity"));
+					orderItem.setUnitPrice(rs.getDouble("unit_price"));
+					// 注入到 orderItems 集合
+					orderItems.add(orderItem);
+				}
+			}
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
