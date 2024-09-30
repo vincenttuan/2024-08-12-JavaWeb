f33e55774eda7a2958925e0369437f76c26c0522
teacher <teacher@BE-403->
2024-09-30 19:36:58 +0800

    web

--- JavaWeb/src/main/java/login/controller/ProductAddServlet.java
+++ JavaWeb/src/main/java/login/controller/ProductAddServlet.java
@@ -3,6 +3,7 @@ package login.controller;
 import java.io.IOException;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,6 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 // 新增商品(含上傳圖片)
 @WebServlet("/product/add")
+@MultipartConfig(maxFileSize = 1024*1024*10) // 設定上傳檔案大小 10MB
 public class ProductAddServlet extends HttpServlet {
 
 	@Override
@@ -20,6 +22,12 @@ public class ProductAddServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 取得一般表單資料
+		String productName = req.getParameter("product_name");
+		String price = req.getParameter("price");
+		String stockQuantity = req.getParameter("stock_quantity");
+		
+		// 取得 type="file" 表單資料
 		
 	}
 	
