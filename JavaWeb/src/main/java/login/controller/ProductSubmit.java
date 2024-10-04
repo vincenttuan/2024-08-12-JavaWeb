fd20225458d6040c96bce189fd4549b4f50a17c2
teacher <teacher@BE-403->
2024-10-04 21:51:36 +0800

    web

--- JavaWeb/src/main/java/login/controller/ProductSubmit.java
+++ JavaWeb/src/main/java/login/controller/ProductSubmit.java
@@ -10,16 +10,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import login.dto.CartDto;
+import login.dto.UserDto;
+import login.service.SalesOrderService;
 
 @WebServlet("/product/submit")
 public class ProductSubmit extends HttpServlet {
-	
+	private SalesOrderService salesOrderService = new SalesOrderService();
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		HttpSession session = req.getSession();
-		
+		UserDto userDto = (UserDto)session.getAttribute("userDto");
 		Map<Integer, CartDto> cart = (Map<Integer, CartDto>)session.getAttribute("cart");
+		salesOrderService.submit(userDto.getUserId(), cart);
+		// 清除購物車 session 資料
+		session.setAttribute("cart", null);
 		
+		resp.getWriter().print("Submit OK");
 	}
 	
 }
