f1bb1143163399cc87082fef52a8c3b8d3e6c9bc
teacher <teacher@BE-403->
2024-10-04 21:08:10 +0800

    web

---
+++ JavaWeb/src/main/java/login/controller/ProductSubmit.java
@@ -0,0 +1,19 @@
+package login.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/product/submit")
+public class ProductSubmit extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+}
